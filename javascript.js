52afa1d4825e42bd4b3897c663949088c57976fe
KunawatV <40499930+KunawatV@users.noreply.github.com>
2018-09-28 17:18:36 +1000

    Return to Normal

--- javascript.js
+++ javascript.js
@@ -1,20 +1,9 @@
-// JavaScript Documentfunction on() {
+// JavaScript Document
 
+var $text = $('.text');
+var $win = $(window);
 
-
-function on() {
-    document.getElementById("overlay").style.display = "block";
-	
-	$('#youtube1')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-	
-	$('#youtube2')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-	
-}
-
-function off() {
-    document.getElementById("overlay").style.display = "none";
-	$('#youtube1')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-	
-	$('#youtube2')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
-	
-}
+$win.on('scroll',function (){
+	var top = $win.scrollTop();
+	$text.css('transform', 'rotate(' + top + 'deg)'); 
+});
