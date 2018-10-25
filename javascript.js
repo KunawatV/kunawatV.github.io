2c74ccba67b95fc03a65792e72e1433f7bec88e4
KunawatV <40499930+KunawatV@users.noreply.github.com>
2018-10-25 12:21:08 +1000

    Math Mate

--- javascript.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-var $text = $('.text');
-var $win = $(window);
-
-$win.on('scroll',function (){
-	var top = $win.scrollTop();
-	$text.css('transform', 'rotate(' + top + 'deg)'); 
-});
