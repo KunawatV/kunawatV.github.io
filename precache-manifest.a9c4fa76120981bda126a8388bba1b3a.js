979066ec18b0b5818758392b61b7df16bd290775
KunawatV <40499930+KunawatV@users.noreply.github.com>
2019-01-10 22:26:18 +0700

    React 1

---
+++ precache-manifest.a9c4fa76120981bda126a8388bba1b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f752a467a9c93cf9ada",
+    "url": "/static/css/main.4dbbd88e.chunk.css"
+  },
+  {
+    "revision": "5f752a467a9c93cf9ada",
+    "url": "/static/js/main.5f752a46.chunk.js"
+  },
+  {
+    "revision": "0e493f63e1ac10c82d37",
+    "url": "/static/js/1.0e493f63.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4b621ef6a3ce4a27896a8209485f37c",
+    "url": "/static/media/20181210-DSC00126.b4b621ef.jpg"
+  },
+  {
+    "revision": "ce5070dc1b1fe73b56d62cf543dea6fd",
+    "url": "/index.html"
+  }
+];
