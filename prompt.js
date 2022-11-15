9402640a198787fae7f620093f15c30841b93e9b
Kunawat <Kunawatvitoorapakorn@gmail.com>
2022-11-15 15:21:10 +1100

    Prompts + explainers

---
+++ prompt.js
@@ -0,0 +1,29 @@
+
+let promptInterval = 10;
+setInterval(myTimer, promptInterval*1000);
+
+let count = 0;
+let max = 4;
+const prompt = [
+    "‿︵‿ Do the wave ︵‿︵",
+    "Do the Dab ヽ(O⌣Oヾ)",
+    "ヽ(O⌣Oヾ) Do it again on the other side",
+    "Hold an invisbile small box □",
+    "Hold an invisible BIG box ◻"
+];
+
+function myTimer() {
+    document.getElementById("prompt").innerHTML = prompt[count]
+    if (count >= max ) {
+        count = 0
+    } else {
+        count += 1;
+    }
+    
+    
+
+
+
+//   const date = new Date();
+// document.getElementById("prompt").innerHTML = date.toLocaleTimeString();
+}
