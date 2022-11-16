ec8e0845bf10881e6e3c716a74efd9e3dbc7642d
Kunawat <Kunawatvitoorapakorn@gmail.com>
2022-11-16 16:02:24 +1100

    Update FINAL form UNI

--- prompt.js
+++ prompt.js
@@ -3,13 +3,15 @@ let promptInterval = 10;
 setInterval(myTimer, promptInterval*1000);
 
 let count = 0;
-let max = 4;
+let max = 6;
 const prompt = [
     "‿︵‿ Do the wave ︵‿︵",
     "Do the Dab ヽ(O⌣Oヾ)",
     "ヽ(O⌣Oヾ) Do it again on the other side",
     "Hold an invisbile small box □",
-    "Hold an invisible BIG box ◻"
+    "Hold an invisible BIG box ◻",
+    "Do the YMCA",
+    "Strike a Pose!"
 ];
 
 function myTimer() {
